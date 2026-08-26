626e7fba3b7b0d59741180c6442ae404e7d4d18b
Matias Parra <matias.parra.rojas@gmail.com>
2026-08-26 17:08:39 -0400

    Amplía ejemplo de tool mínima

--- slides/202608-taller-usuarios-R/ellmer/08-tool-minima.R
+++ slides/202608-taller-usuarios-R/ellmer/08-tool-minima.R
@@ -1,13 +1,18 @@
-desocupacion_desde_2020 <- function() {
-  get_series("F049.DES.TAS.INE9.10.M", from = "2020-01-01")
+desocupacion_full <- function() {
+  get_series("F049.DES.TAS.INE9.10.M")
 }
 
 tool_minima <- tool(
-  desocupacion_desde_2020,
-  description = "Descarga la tasa de desocupación nacional mensual desde 2020."
+  desocupacion_full,
+  description = "Descarga la tasa de desocupación nacional sin ajuste estacional"
 )
 
 chat_minimo <- chat_openai(echo = TRUE)
 chat_minimo$register_tool(tool_minima)
 
-chat_minimo$chat("¿Cómo ha evolucionado la desocupación en Chile desde 2020?")
+chat_minimo$chat(
+  glue(
+    "Consulta la tasa de desocupación de Chile.",
+    "Luego explica cómo ha evolucionado desde 2020 usando los datos."
+  )
+)
